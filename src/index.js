aca2010988c939195db4af8d4fdc9bce8e8a8b61
Andrey <rimmon9@gmail.com>
2023-01-26 11:20:27 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -34,13 +34,31 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+    '-----': '0',
+    '**********': ' ',
 };
 
-function decode(expr) {
-    // write your solution here
+const decode = (expr)=> {
+    const array = expr.match(/.{1,10}/g);
+    let arr = []
+    array.forEach(element => {
+        arr.push(element.replace(/10/gi,'.').replace(/11/gi,'-').replace(/0/gi,''))
+    });
+    let ar = []
+        for (let i = 0; i < arr.length; i++) {
+            for (let key in MORSE_TABLE) {
+        if (arr[i]==key) {
+            ar.push(MORSE_TABLE[key])
+        }
+    }
+}
+    return ar.join('')
 }
 
-module.exports = {
-    decode
-}
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"));
+
+
+
+// module.exports = {
+//     decode
+// }
